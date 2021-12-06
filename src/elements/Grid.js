e6020ec4a1371e069300b031db3b2a699de24a8c
cslim0527@gmail.com <cslim0527@gmail.com>
2021-12-06 18:26:55 +0900

    [ADD] Grid Element 추가

---
+++ src/elements/Grid.js
@@ -0,0 +1,24 @@
+import styled from 'styled-components'
+
+const Grid = (props) => {
+  const { is_container, children } = props
+  const styles = {
+    is_container
+  }
+
+  return (
+    <GridBox {...styles}>
+      { children }
+    </GridBox>
+  )
+}
+
+Grid.defaultProps = {
+  is_container: false
+}
+
+const GridBox = styled.div`
+  ${(props) => props.is_container && `max-width: 1264px; margin: 0 auto;`}
+`
+
+export default Grid
--- src/pages/PostList.js
+++ src/pages/PostList.js
@@ -1,10 +1,11 @@
 import React from 'react'
+import { Grid } from '../elements'
 
 const PostList = () => {
   return (
-    <div>
+    <Grid is_container>
       포스트 리스트 입니다.
-    </div>
+    </Grid>
   )
 }
 
