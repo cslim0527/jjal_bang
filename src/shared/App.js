aa990916a1ffc81e3e7014560b249a7959fddcf9
leejh4197 <leejh4197@naver.com>
2021-12-08 16:55:05 +0900

    lodash패키지 설치와 Header의 Input에 debounce 기능 추가

--- src/shared/App.js
+++ src/shared/App.js
@@ -8,16 +8,20 @@ import PostDetail from '../pages/PostDetail'
 import LikeList from '../pages/LikeList'
 import Login from '../pages/Login'
 import Signup from '../pages/Signup'
+import Header from '../components/Header'
+import Footer from '../components/Footer' 
 
 function App() {
 
   return (
     <div className="App">
+      <Header/>
       <GlobalStyles/>
       <Route path="/" component={PostList} exact />
       <Route path="/login" component={Login} exact />
       <Route path="/signup" component={Signup} exact />
       <Route path="/write" component={PostWrite} exact />
+      <Footer/>
     </div>
   )
 }
